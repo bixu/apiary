9992b14120834a05e879a1f7ff2125fee77123b1
Bastian Gruber <foreach@me.com>
2023-03-18 07:23:05 +0100

    Upgrade to clap 4.1.8 (#2)
    * Upgrade to clap 4.1.8
    
    * Add jq example to README
    
    * Turn off unused `blocking` feature flag for `reqwest`
    
    ---------
    
    Co-authored-by: Blake Irvin <blakeirvin@me.com>

--- README.md
+++ README.md
@@ -3,3 +3,31 @@
 `apiary` is a command-line interface to the Honeycomb.io API, written in Rust.
 This is the first Rust program I've ever written.
 Constructive criticism is most welcome!
+
+## Usage
+
+Getting help via the `--help` command:
+
+```bash
+$ cargo run -- --help
+    Finished dev [unoptimized + debuginfo] target(s) in 0.05s
+     Running `target/debug/apiary --help`
+A command-line interface to the Honeycomb API
+
+Usage: apiary --api-key <API_KEY> --dataset <DATASET> --resource <RESOURCE>
+
+Options:
+  -a, --api-key <API_KEY>
+  -d, --dataset <DATASET>
+  -r, --resource <RESOURCE>
+  -h, --help                 Print help
+  -V, --version              Print version
+```
+
+Try using piping output to `jq` like this:
+
+```bash 
+ $ apiary --api_key=$HONEYCOMB_API_KEY --dataset="<dataset name>" --resource="columns" \
+         | jq --raw-output '.[] | "\(.key_name) \(.id)"';
+
+```
--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,31 @@
-use clap::{Arg, App};
+use clap::Parser;
 use reqwest;
 use tokio;
 
-struct ProgramArgs {
+#[derive(Parser)]
+#[command(name = "apiary")]
+#[command(author = "Blake")]
+#[command(version = "0.2.0")]
+#[command(about = "A command-line interface to the Honeycomb API", long_about = None)]
+struct Cli {
+    #[arg(short, long)]
     api_key: String,
+    #[arg(short, long)]
     dataset: String,
+    #[arg(short, long)]
     resource: String,
 }
 
-impl ProgramArgs {
-    fn new() -> Self {
-        
-        let about_text = r###"A command-line interface to the Honeycomb API
-
-Try using piping output to `jq` like this:
-
-    apiary --api_key=$HONEYCOMB_API_KEY --dataset="<dataset name>" --resource="columns" \
-        | jq --raw-output '.[] | "\(.key_name) \(.id)"'    
-
-"###;
-
-        // basic app information
-        let app = App::new("apiary")
-            .version("0.1.0")
-            .about(about_text);
-
-        let api_key_option = Arg::with_name("api_key")
-            .long("api_key") // allow --name
-            .short("k") // allow -n
-            .takes_value(true)
-            .help("A valid Honeycomb API key.")
-            .required(true);
-
-        let dataset_option = Arg::with_name("dataset")
-            .long("dataset")
-            .short("d")
-            .takes_value(true)
-            .help("The Honeycomb Dataset to query.")
-            .required(true);
-
-        let resource_option = Arg::with_name("resource")
-            .long("resource")
-            .short("r")
-            .takes_value(true)
-            .help("The Honeycomb Dataset resource to interact with.")
-            .required(true);
-
-        let app = app.arg(api_key_option);
-        let app = app.arg(dataset_option);
-        let app = app.arg(resource_option);
-
-        let matches = app.get_matches();
-
-        let api_key = matches.value_of("api_key")
-            .expect("You must pass in a valid API key with `--api_key`.");
-
-        let dataset = matches.value_of("dataset")
-            .expect("You must pass in a Dataset with `--dataset`.");
-
-        let resource = matches.value_of("resource")
-            .expect("You must pass in a Dataset resource with `--resource`.");
-
-        ProgramArgs { 
-            api_key: api_key.to_string(),
-            dataset: dataset.to_string(),
-            resource: resource.to_string(),
-        }
-    }
-}
-
-
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    let args = ProgramArgs::new();
+    let cli = Cli::parse();
 
-    let url = format!("https://api.honeycomb.io/1/{}/{}", args.resource, args.dataset);
+    let url = format!("https://api.honeycomb.io/1/{}/{}", cli.resource, cli.dataset);
 
     let client = reqwest::Client::new();
     let response = client
         .get(url)
-        .header("x-honeycomb-team", args.api_key)
+        .header("x-honeycomb-team", cli.api_key)
         .send()
         .await
         .expect("failed to get response")
