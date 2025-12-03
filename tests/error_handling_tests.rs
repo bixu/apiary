88ee12e01174b43fd7d95c013ed45b8073fd3e90
Blake Irvin <blakeirvin@me.com>
2025-12-03 08:59:20 +0100

    Move insecure url env var to Actions workflow to keep our Rust shiny

--- tests/error_handling_tests.rs
+++ tests/error_handling_tests.rs
@@ -59,10 +59,6 @@ mod authentication {
 
     #[tokio::test]
     async fn test_missing_required_key() {
-        // Allow insecure HTTP URLs for this test
-        unsafe {
-            std::env::set_var("ALLOW_INSECURE_HONEYCOMB_TEST_URLS", "true");
-        }
         let client = HoneycombClient::new(None, None, Some("http://api.test".to_string()));
 
         // Should fail without any API keys
@@ -267,10 +263,6 @@ mod connectivity {
 
     #[tokio::test]
     async fn test_connection_timeout() {
-        // Allow insecure HTTP URLs for this test
-        unsafe {
-            std::env::set_var("ALLOW_INSECURE_HONEYCOMB_TEST_URLS", "true");
-        }
         // Use a non-routable IP to simulate connection timeout
         let client = HoneycombClient::new(
             None,
@@ -292,10 +284,6 @@ mod connectivity {
 
     #[tokio::test]
     async fn test_invalid_hostname() {
-        // Allow insecure HTTP URLs for this test
-        unsafe {
-            std::env::set_var("ALLOW_INSECURE_HONEYCOMB_TEST_URLS", "true");
-        }
         let client = HoneycombClient::new(
             None,
             Some("test-key".to_string()),
